728a79ff0b8f82938577cf67ca1fc5902c11bd8c
Aksha Patel <aksharpatel.dev@gmail.com>
2025-01-04 13:56:24 -0400

    Build a responsive layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { Button } from '@/components/ui/button';
-import { HStack } from '@chakra-ui/react';
+import { Grid, GridItem, Stack } from '@chakra-ui/react';
 
 const App = () => {
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    // breakpoints
+    //   "2xl": "96em" --> 4K
+    //   xl: "80em" --> Laptop
+    //   lg: "62em" --> Tablet
+    //   md: "48em" --> Medium Mobile
+    //   sm: "30em" --> Small Mobile
+    //   base: "0em" --> 0px
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area='nav' bg='coral'>
+        Nav
+      </GridItem>
+      <Stack hideBelow='lg'>
+        {/* Only show this item in xl and 2xl */}
+        <GridItem area='aside' bg='gold'>
+          Aside
+        </GridItem>
+      </Stack>
+      <GridItem area='main' bg='dodgerblue'>
+        Main
+      </GridItem>
+    </Grid>
   );
 };
 
