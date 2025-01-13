9cb3640c21c7053f2e5a7940247c77d0d81e944c
Aksha Patel <aksharpatel.dev@gmail.com>
2025-01-12 20:04:30 -0400

    Build sort selector

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,53 @@
+import {
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+} from '@/components/ui/menu';
+import { Group, IconButton } from '@chakra-ui/react';
+import { Button } from '@/components/ui/button';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
+import { useState } from 'react';
+
+const SortSelector = () => {
+  const [isExpended, setIsExpended] = useState<Boolean>(false);
+  const platforms = [
+    'Relevance',
+    'Date added',
+    'Name',
+    'Release date',
+    'Popularity',
+    'Average rating',
+  ];
+
+  return (
+    <MenuRoot
+      positioning={{ placement: 'right-start' }}
+      onInteractOutside={() => setIsExpended(false)}
+    >
+      <MenuTrigger asChild>
+        <Group attached onClick={() => setIsExpended(!isExpended)}>
+          <Button variant='outline' size='sm'>
+            Order by: Relevance
+          </Button>
+          <IconButton variant='outline' size='sm'>
+            {isExpended ? <BsChevronLeft /> : <BsChevronRight />}
+          </IconButton>
+        </Group>
+      </MenuTrigger>
+      <MenuContent>
+        {platforms.map((platform, i) => (
+          <MenuItem
+            key={i}
+            value={platform}
+            onClick={() => setIsExpended(false)}
+          >
+            {platform}
+          </MenuItem>
+        ))}
+      </MenuContent>
+    </MenuRoot>
+  );
+};
+
+export default SortSelector;
