7027f3a566e495d98e8684f1b26f5967a11bf823
Aksha Patel <aksharpatel.dev@gmail.com>
2025-01-13 19:07:24 -0400

    Adding sorting logic to sort selector

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -9,23 +9,31 @@ import { Button } from '@/components/ui/button';
 import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
 import { useState } from 'react';
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
   const [isExpended, setIsExpended] = useState<Boolean>(false);
-  const platforms = [
-    'Relevance',
-    'Date added',
-    'Name',
-    'Release date',
-    'Popularity',
-    'Average rating',
+  const sortOrders = [
+    { value: '', label: 'Relevance' },
+    { value: '-added', label: 'Date added' },
+    { value: 'name', label: 'Name' },
+    { value: '-released', label: 'Release date' },
+    { value: '-metacritic', label: 'Popularity' },
+    { value: '-rating', label: 'Average rating' },
   ];
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
 
   return (
     <MenuRoot onInteractOutside={() => setIsExpended(false)}>
       <MenuTrigger asChild>
         <Group attached onClick={() => setIsExpended(!isExpended)}>
           <Button variant='outline' size='sm'>
-            Order by: Relevance
+            Order by: {currentSortOrder?.label || 'Relevance'}
           </Button>
           <IconButton variant='outline' size='sm'>
             {isExpended ? <BsChevronDown /> : <BsChevronUp />}
@@ -33,13 +41,13 @@ const SortSelector = () => {
         </Group>
       </MenuTrigger>
       <MenuContent>
-        {platforms.map((platform, i) => (
+        {sortOrders.map((order) => (
           <MenuItem
-            key={i}
-            value={platform}
-            onClick={() => setIsExpended(false)}
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+            value={order.value}
           >
-            {platform}
+            {order.label}
           </MenuItem>
         ))}
       </MenuContent>
