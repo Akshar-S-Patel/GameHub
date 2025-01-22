e9a7ac2f44a4b814ba3e0ab570789dc13e6dc8d1
Aksha Patel <aksharpatel.dev@gmail.com>
2025-01-21 23:17:23 -0400

    Adding DevTools for tanstack react query

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from '@/components/ui/provider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
 import './index.css';
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
     <Provider>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </Provider>
   </StrictMode>
