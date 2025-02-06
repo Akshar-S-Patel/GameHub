a954a1b3c53f2b09b1314f79596caab462d0c10c
Aksha Patel <aksharpatel.dev@gmail.com>
2025-02-05 20:34:14 -0400

    Refactor PlatformIconList to use SiNintendo icon and simplify rendering logic

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -7,21 +7,22 @@ import {
   FaAndroid,
 } from 'react-icons/fa';
 import { MdPhoneIphone } from 'react-icons/md';
-import { BsGlobe, BsNintendoSwitch } from 'react-icons/bs';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
 import { HStack, Icon } from '@chakra-ui/react';
-import Platform from '@/entities/Platform';
+import Platform from '../entities/Platform';
 import { IconType } from 'react-icons';
 
 interface Props {
   platforms: Platform[];
 }
 
-const PlatformIconList = ({ platforms }: Props) => {
+const PlatformIconList = ({ platforms = [] }: Props) => {
   const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
     xbox: FaXbox,
-    nintendo: BsNintendoSwitch,
+    nintendo: SiNintendo,
     mac: FaApple,
     linux: FaLinux,
     android: FaAndroid,
@@ -31,15 +32,9 @@ const PlatformIconList = ({ platforms }: Props) => {
 
   return (
     <HStack marginY={1}>
-      {platforms.map((platform) => {
-        const IconComponent = iconMap[platform.slug];
-
-        return (
-          <Icon key={platform.id} color='gray.500'>
-            <IconComponent size={10} />
-          </Icon>
-        );
-      })}
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500' />
+      ))}
     </HStack>
   );
 };
